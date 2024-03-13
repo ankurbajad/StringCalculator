e9eba45e720d2a2c53e71609cc494ecaccc9fcfb
ankur bajad <ankurbajad786@gmail.com>
2024-03-13 18:29:29 +0530

    added method for handle invalid input

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,10 +1,17 @@
 require 'minitest/autorun'
 
 class StringCalculator
+
   def self.add(numbers)
-    return 0 if numbers.empty?
-    numbers.split(/[,\n]/).map(&:to_i).sum
+    if numbers.empty?
+      return 0
+    elsif numbers.match?(/,\n|\n,|\n\n/)
+      return "Invalid input"
+    end
+
+    numbers.split(/[,\n]+/).map(&:to_i).sum
   end
+
 end
 
 class TestStringCalculator < Minitest::Test
@@ -26,7 +33,7 @@ class TestStringCalculator < Minitest::Test
   end
 
   def test_handles_invalid_input_with_newline_after_comma
-    assert_equal 1, StringCalculator.add("1,\n")
+    assert_equal "Invalid input", StringCalculator.add("1,\n")
   end
 
 end
